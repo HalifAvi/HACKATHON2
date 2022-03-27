084d758de63e188fb3d96d9c969c2fa72d3e93ed
HalifAvi <96798674+HalifAvi@users.noreply.github.com>
2022-03-27 14:31:28 +0300

    login + register work well

---
+++ modules/db.js
@@ -0,0 +1,78 @@
+const knex = require('knex');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const db = knex({
+
+    client: 'pg',
+    connection:{
+
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
+    }
+});
+
+
+/*
+*
+* This function get the user's details from db (returns a Promise)
+*/
+const getUser = (obj) => {
+
+    let condition;
+
+    // Check if we just want to know if we already have the same username in db
+    // Or we want to know thw username + password are correct to login 
+    obj.hasOwnProperty('password') ? condition = {username : obj.username, password : obj.password} :
+                                     condition = {username : obj.username}       
+
+    return db('Hackaton2')
+    .select('*')
+    .from('users')
+    .where(condition)
+} 
+
+
+/*
+*
+* This function insert new user to the db
+*/
+const insertNewUserToDB = (obj) => {
+
+    db('Hackaton2')
+   .insert([{user_name : obj.firstName, user_last_name : obj.lastName, age : obj.age, e_mail : obj.email, password : obj.password, username : obj.username}])
+   .from('users')
+   .catch(e => console.log(e))
+}
+
+
+/*
+*
+* This function insert new user status to the db
+*/
+// const insertUserStatusToDB = (status, userID) => {
+
+//     status = "online"; // For new user
+
+//     db('Hackaton2')
+//    .insert([{status_name : status, }])
+//    .from('usersStatus')
+//    .catch(e => console.log(e))
+// }
+
+
+module.exports = {
+
+    getUser, 
+    insertNewUserToDB,
+    // insertUserStatusToDB
+}
+
+
+
+
+
---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+//** Get 'accountCreated' info */ 
+let strDataFromForm = document.querySelector('div.join-container').id;
+console.log(strDataFromForm)
+
+
+if(strDataFromForm === "true"){
+
+    alert("Your account has been successfully created! :)");
+}
